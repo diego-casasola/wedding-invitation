6592b22e289e51fe712a14a695526dc0179685a7
Diego Casasola <diegoandrecf.dacf@gmail.com>
2024-09-16 09:06:52 -0400

    DC - new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,91 +2,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import styles from "../app/styles/Home.module.css"
+import styles from "../app/styles/Main.module.css"
+import json_wedding_data from "../data/wedding-data.json"
+import { Clock } from "@/components";
 
 export default function Home() {
-  const [partyTime, setPartyTime] = useState(false);
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-
-  useEffect(() => {
-    const target = new Date("11/17/2024 18:00:00");
-
-    const interval = setInterval(() => {
-      const now = new Date();
-      const difference = target.getTime() - now.getTime();
-
-      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-      setDays(d);
-
-      const h = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      setHours(h);
-
-      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      setMinutes(m);
-
-      const s = Math.floor((difference % (1000 * 60)) / 1000);
-      setSeconds(s);
-
-      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
-        setPartyTime(true);
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
+  const data = json_wedding_data;
+  console.log(data);
+  return(
     <div className={styles.container}>
-      <Head>
-        <title>Countdown Timer</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {partyTime ? (
-        <>
-          <h1>Happy new year!</h1>
-          <video autoPlay loop muted>
-            <source src="/party.mp4" />
-          </video>
-        </>
-      ) : (
-        <>
-          <div className="timer-wrapper">
-            <div className="timer-inner">
-              <div className="timer-segment">
-                <span className="time">{days}</span>
-                <span className="label">Days</span>
-              </div>
-              <span className="divider">:</span>
-              <div className="timer-segment">
-                <span className="time">{hours}</span>
-                <span className="label">Hours</span>
-              </div>
-              <span className="divider">:</span>
-              <div className="timer-segment">
-                <span className="time">{minutes}</span>
-                <span className="label">Minutes</span>
-              </div>
-              <span className="divider">:</span>
-              <div className="timer-segment">
-                <span className="time">{seconds}</span>
-                <span className="label">Seconds</span>
-              </div>
-            </div>
-          </div>
-          <Image
-            alt="background image"
-            src="/image.webp"
-            layout="fill"
-            quality={100}
-          />
-        </>
-      )}
+      <div className={styles.border}>
+        <h1>This is the clock</h1>
+        <Clock></Clock>
+      </div>
     </div>
-  );
+  )
 }
---
+++ src/components/ui/clock.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import styles from "../../app/styles/Home.module.css"
+import Head from "next/head";
+
+export const Clock = () => {
+    const [partyTime, setPartyTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const target = new Date("11/17/2024 18:00:00");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+      const difference = target.getTime() - now.getTime();
+
+      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+      setDays(d);
+
+      const h = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      setHours(h);
+
+      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      setMinutes(m);
+
+      const s = Math.floor((difference % (1000 * 60)) / 1000);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setPartyTime(true);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Countdown Timer</title>
+      </Head>
+        <>
+          <div className="timer-wrapper">
+            <div className="timer-inner">
+              <div className="timer-segment">
+                <span className="time">{days}</span>
+                <span className="label">Días</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{hours}</span>
+                <span className="label">Horas</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{minutes}</span>
+                <span className="label">Minutos</span>
+              </div>
+              <span className="divider">:</span>
+              <div className="timer-segment">
+                <span className="time">{seconds}</span>
+                <span className="label">Segundos</span>
+              </div>
+            </div>
+          </div>
+        </>
+    </div>
+  );
+};
+
+export default Clock;
